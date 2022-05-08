0bb781d18e18cd5eb4b46b599ba2cc05cc043987
jnani143 <99253160+jnani143@users.noreply.github.com>
2022-05-08 13:21:49 +0530

    Create no for odd keys and yes for even numbers.py

---
+++ no for odd keys and yes for even numbers.py
@@ -0,0 +1,11 @@
+n=int(input())
+m={}
+for i in range(1,n+1):
+    if i%2==0:
+        m[i]='yes'
+    else:
+        m[i]='no'
+print(m)
+
+8
+{1: 'no', 2: 'yes', 3: 'no', 4: 'yes', 5: 'no', 6: 'yes', 7: 'no', 8: 'yes'}
